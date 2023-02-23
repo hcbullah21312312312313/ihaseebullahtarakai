e03f41fa36b58cb9b280f9fe77bfd53fb84d4f8c
Haseeb <haseebullah7aand7b@gmail.com>
2023-02-23 12:38:04 +0500

    issues fixed

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ app.use(express.static("public"))
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
-app.listen(process.env.PORT || 3000, function() { console.log("Server started successfully") })
+const port = process.env.PORT || 3000;
+app.listen(port, function() { console.log("Server started successfully") })
