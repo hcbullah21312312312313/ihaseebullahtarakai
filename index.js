cb97e429e7f4032c84616859f3ba40335730fbbe
Haseeb <haseebullah7aand7b@gmail.com>
2023-02-23 13:04:35 +0500

    issue 1 fixed

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ app.use(express.static("public"))
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
-const port = process.env.PORT || 3000;
-app.listen(port, function() { console.log("Server started successfully") })
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+});
